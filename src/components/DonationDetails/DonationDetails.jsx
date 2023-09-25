c740b47efc6c7e76d3c4b3489b3afa516f37e2cf
torikulswe <torikulswe@gmail.com>
2023-09-25 19:40:01 +0600

    title and description added for donation details page

--- src/components/DonationDetails/DonationDetails.jsx
+++ src/components/DonationDetails/DonationDetails.jsx
@@ -9,21 +9,23 @@ const DonationDetails = () => {
   const donation = donations.find((donation) => donation.id === idInt);
   //   console.log(donation);
   return (
-    <div>
-      <h2>Donation details:{id}</h2>
+    <div className="px-4 lg:px-0">
       <div className="relative">
         <img
           src={donation.picture}
           alt="Donation"
-          className="w-full lg:h-[800px] rounded-lg"
+          className="w-full h-full rounded-lg"
         />
+        <div className="absolute mx-auto bottom-0 left-0 right-0 top-auto h-24 bg-black opacity-40"></div>
 
-        <div className="absolute bottom-0 p-6 flex items-center justify-center">
+        <div className="absolute bottom-0  p-6 flex items-center justify-center">
           <button className="bg-[#FF444A] text-white px-4 py-2 rounded">
             Donate ${donation.price}
           </button>
         </div>
       </div>
+      <h2 className="text-[40px] font-bold mt-14 mb-6">{donation.title}</h2>
+      <p className="pb-40">{donation.description}</p>
     </div>
   );
 };
