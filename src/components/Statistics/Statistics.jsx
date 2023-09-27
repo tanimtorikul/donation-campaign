f2f7488351f29ffdb4444984d621393711422ae2
torikulswe <torikulswe@gmail.com>
2023-09-27 19:13:14 +0600

    statistics component fixed

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -12,10 +12,22 @@ const Statistics = () => {
   }, []);
   const yourDonation = donationData.reduce((total, item) => total + 1, 0);
 
+  const percentageYourDonation = ((yourDonation / 12) * 100).toFixed(2);
+  const percentageTotalDonation = (((12 - yourDonation) / 12) * 100).toFixed(2);
+
   const data = [
-    { name: "Your Donation", value: yourDonation },
-    { name: "Total Donation", value: 12 - yourDonation },
+    {
+      name: "Your Donation",
+      value: yourDonation,
+      percentage: percentageYourDonation,
+    },
+    {
+      name: "Total Donation",
+      value: 12 - yourDonation,
+      percentage: percentageTotalDonation,
+    },
   ];
+
   console.log(yourDonation);
 
   const COLORS = ["#FF444A", "#00C49F"];
@@ -45,12 +57,13 @@ const Statistics = () => {
         </div>
         <div className="legend flex justify-center gap-6">
           {data.map((entry, index) => (
-            <div key={`legend-${index}`} className="flex items-center mb-2">
+            <div key={`legend-${index}`} className="flex items-center mb-2 ">
               <div className="text-sm">{entry.name}</div>
               <div
-                className="w-24 h-4 mr-2"
+                className="lg:w-24 h-4 mr-2"
                 style={{ backgroundColor: COLORS[index % COLORS.length] }}
               ></div>
+              <div className="text-sm">{entry.percentage}%</div>
             </div>
           ))}
         </div>
